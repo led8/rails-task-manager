187e71c110e69f31607c039170b737476e18a52d
Arthur Dhuy <a.dhuy@orange.fr>
2019-01-30 09:41:38 +0100

    Controller refacto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,18 +19,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params_to_update)
     @task.save
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
   end
 
@@ -41,5 +39,9 @@ class TasksController < ApplicationController
 
   def task_params_to_update
     params.require(:task).permit(:title, :details, :completed, :progress)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
